7077376f762c2b684dddf4cdcb85cd463d0d9324
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-02-13 16:54:34 +0200

    Refactor Unit Testing

--- test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
+++ test/src/features/authentication/presentation/sign_in/email_password_sign_in_controller_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAuthRepository extends Mock implements FakeAuthRepository {}
+import '../account/account_screen_controller_test.dart';
 
 void main() {
   const testEmail = 'test@test.com';
