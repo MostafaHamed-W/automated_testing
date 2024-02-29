7b1bdb34d9495d3dc5376d3c68855c43e72781a0
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-02-29 19:41:42 +0200

    Testing AccountScreenController with Mocks

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,9 +1,8 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue> {
-  AccountScreenController({required this.authRepository})
-      : super(const AsyncValue.data(null));
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+  AccountScreenController({required this.authRepository}) : super(const AsyncValue.data(null));
   final FakeAuthRepository authRepository;
 
   Future<void> signOut() async {
@@ -13,8 +12,7 @@ class AccountScreenController extends StateNotifier<AsyncValue> {
 }
 
 final accountScreenControllerProvider =
-    StateNotifierProvider.autoDispose<AccountScreenController, AsyncValue>(
-        (ref) {
+    StateNotifierProvider.autoDispose<AccountScreenController, AsyncValue>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AccountScreenController(
     authRepository: authRepository,
