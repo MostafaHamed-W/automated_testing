2f0a0f9aca2a59a7b264f6033ba5d22eeb870a03
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-02-29 15:42:21 +0200

    Refactor products repository testing with setUpAll

--- test/src/features/products/data/fake_products_repository_test.dart
+++ test/src/features/products/data/fake_products_repository_test.dart
@@ -1,12 +1,23 @@
+import 'dart:math';
+
 import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  late FakeProductsRepository fakeProductsRepository;
+  setUpAll(() {
+    fakeProductsRepository = FakeProductsRepository();
+  });
+
   group('Test Prodcuts', () {
-    test('get procut list funtion', () {
-      final FakeProductsRepository fakeProductsRepository = FakeProductsRepository();
-      expect(kTestProducts, fakeProductsRepository.getProductsList());
+    test('get procuts list funtion', () {
+      expect(fakeProductsRepository.getProductsList(), kTestProducts);
+    });
+
+    test('get product with id', () {
+      expect(fakeProductsRepository.getProduct('1'), kTestProducts[0]);
     });
   });
 }
